ed69887c66e2a1790e4a0a6e7783c118c0ff3f86
Estefany Ochoa Romero <tefaoch23@gmail.com>
2026-03-20 20:11:14 +0100

    He borrado el metodo DiasenMesArray y el print de este de forma segura

--- src/Tarea4/Tarea4.java
+++ src/Tarea4/Tarea4.java
@@ -4,10 +4,10 @@ package Tarea4;
 public class Tarea4 {
 
     public static void main(String[] args) {
-        System.out.println("Febrero de 2026 tiene " + Utilidades.diasEnMesArray(2, 2026) + " días");
+        
         System.out.println("60mph son " + Utilidades.kmhAms(Utilidades.mphAKmh(60)) + " metros por segundo");
         System.out.println("Si el cateto A mide 3cm y el cateto B mide 4cm, entonces la hipotenusa mide "
-                + Utilidades.hip(3, 4) + "cm");
+                + Utilidades.hipotenusa(3, 4) + "cm");
     }
 
 }
